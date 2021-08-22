18aac736519e5b1a9e13859ba242be9879b280bf
lingzihao23 <68987717+lingzihao23@users.noreply.github.com>
2021-08-23 00:42:38 +0800

    Add files via upload

---
+++ MenuDetails.aspx
@@ -0,0 +1,106 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true" CodeBehind="MenuDetails.aspx.cs" Inherits="Web_Asgm.ArtDetails" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" Runat="server">
+    
+    <link href="MenuDetails.css" rel="stylesheet" type="text/css">
+    <br /><br /><br /><br />
+
+    <asp:DataList ID="artDetailsList" runat="server" DataKeyField="ArtID" DataSourceID="SqlDataSource1">
+        <ItemTemplate>
+            <br />
+
+            <div align="center">
+                <hErrorQuan>
+                    <asp:Label ID="validateQuanLbl" runat="server" Text=""></asp:Label>
+                </hErrorQuan>
+            </div>
+
+            <br />
+
+            <div align="center">
+                <hView>
+                    <asp:Label ID="nowViewlbl" runat="server" Text="You are now viewing"></asp:Label>
+                </hView>
+            </div>
+
+            <br />
+
+            <div align="center">
+                <hArtNameLbl>
+                    <asp:Label ID="artNamelbl" runat="server" Text='<%# Eval("ArtName") %>'></asp:Label>
+                </hArtNameLbl>
+                <br />
+                <hArtIdlbl>
+                    <asp:Label ID="artIDlbl" runat="server" Text='<%# Eval("ArtID") %>'></asp:Label>
+                </hArtIdlbl>
+            </div>
+            
+            <table class="detailTable">
+                <tr>
+                    <td class="detailImg" rowspan="5">
+                        <asp:Image ID="Image1" runat="server" Height="400px" ImageUrl='<%# Eval("ArtPic") %>' Width="400px" />
+                    </td>
+                    <td class="detailInfo">Artwork Name:</td>
+                    <td class="detailInfo">
+                        <asp:Label ID="viewArtNamelbl" runat="server" Text='<%# Eval("ArtName") %>'></asp:Label>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td class="detailInfo">Price:</td>
+                    <td>RM
+                        <asp:Label ID="viewPricelbl" runat="server" Text='<%# Eval("Price") %>'></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="detailInfo">Pieces Available:</td>
+                    <td>
+                        <asp:Label ID="viewStocklbl" runat="server" Text='<%# Eval("Stock") %>'></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="detailInfo">Product Description:</td>
+                    <td>
+                        <asp:Label ID="viewArtDesc" runat="server" Text='<%# Eval("ArtDesc") %>'></asp:Label>
+                    </td>
+                </tr>
+            </table>
+            <br /><br />
+            <table class="detailBtnTable" align="center">
+                <tr>
+                   <td class="detailBtn"><asp:ImageButton ID="btnAddWish" runat="server" Text="Add To Wishlist" ImageUrl="media files/wishlistAdd.png" CommandArgument='<%#Eval("ArtID") %>' OnClick="btnAddWish_Click" CommandName="AddWishlist" CssClass="detailBtn" Session="addWishlist"></asp:ImageButton></td>
+                </tr>
+                <tr><td>&nbsp;</td></tr>
+                <tr><td>&nbsp;</td></tr>
+                <tr><td>&nbsp;</td></tr>
+                <tr>
+                    <td class="quantityBox">
+                        Enter quantity: <asp:TextBox ID="quantityText" runat="server" TextMode="Number" placeholder='<%# Eval("Stock") %>' AutoPostBack="False"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr><td>&nbsp;</td></tr>
+                <tr>
+                    <td class="detailBtn"><asp:linkButton ID="btnAddCart" runat="server" Text="Add To Cart" OnClick="btnAddCart_Click" /></td>
+                </tr>
+            </table>
+        </ItemTemplate>
+    </asp:DataList>
+    
+    <br />
+    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" SelectCommand="SELECT * FROM ArtGalleryData INNER JOIN UserDetail ON ArtGalleryData.UserID = UserDetail.UserID WHERE ([ArtID] = @ArtID)">
+        <SelectParameters>
+            <asp:QueryStringParameter Name="ArtID" QueryStringField="id" Type="Int32" />
+        </SelectParameters>
+</asp:SqlDataSource>
+    <br />
+    <br />
+    <br />
+    <br />
+
+</asp:Content>
+
+
+
+
+
+
---
+++ MenuDetails.aspx.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace Web_Asgm
+{
+    public partial class ArtDetails : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["UserID"] == null)
+            {
+                Response.Redirect("Login.aspx");
+            }
+
+            foreach (DataListItem Item in artDetailsList.Items)
+            {
+
+                ImageButton btnAddWish = (ImageButton)Item.FindControl("btnAddWish");
+                Label artIDlbl = (Label)Item.FindControl("artIDlbl");
+                int artID = int.Parse(artIDlbl.Text);
+                string strCon = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+                SqlConnection connect;
+                connect = new SqlConnection(strCon);
+                connect.Open();
+                SqlDataAdapter da = new SqlDataAdapter("Select artID From WishlistData where artID=" + artID, connect);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                if (dt.Rows.Count.ToString() == "0")
+                {
+                    btnAddWish.ImageUrl = "media files/wishlistAdd.png";
+                }
+                else
+                {
+                    btnAddWish.ImageUrl = "media files/wishlistAdded.png";
+                    btnAddWish.Enabled = false;
+                }
+
+            }
+        }
+
+        protected void btnAddCart_Click(object sender, EventArgs e)
+        {
+    
+
+            foreach (DataListItem Item in artDetailsList.Items)
+            {
+                TextBox quantityText = (TextBox)Item.FindControl("quantityText");
+                LinkButton btnAddCart = (LinkButton)Item.FindControl("btnAddCart");
+                Label viewStocklbl = (Label)Item.FindControl("viewStocklbl");
+                Label artIDlbl = (Label)Item.FindControl("artIDlbl");
+                Label validateQuanLbl = (Label)Item.FindControl("validateQuanLbl");
+
+                if (quantityText.Text == string.Empty)
+                {
+                    validateQuanLbl.Text = "No value entered. Try again.";
+                    Response.Write("<script language=javascript>alert('No value entered. Try again.')</script>");
+                }
+                else
+                {
+                    int quanSelected = int.Parse(quantityText.Text);
+                    int checkStock = int.Parse(viewStocklbl.Text);
+                    int artId = int.Parse(artIDlbl.Text);
+                    
+                    if (quanSelected <= checkStock && quanSelected > 0 && quanSelected != 0)
+                    {
+                        string strCon = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+                        SqlConnection connect;
+                        connect = new SqlConnection(strCon);
+                        SqlConnection connect2;
+                        connect2 = new SqlConnection(strCon);
+
+                        connect.Open();
+                        connect2.Open();
+
+                        SqlCommand cartCheck = new SqlCommand("Select * from CartData where UserID =" + Session["UserID"] + "and ArtID =" + artId, connect);
+                        SqlDataReader reader = cartCheck.ExecuteReader();
+                        if (reader.HasRows)
+                        {
+                            SqlCommand cartUpdate = new SqlCommand("Update CartData Set quantity = quantity + " + quanSelected + "where UserID = " + Session["UserID"] + "and ArtID = " + artId, connect2);
+                            cartUpdate.ExecuteNonQuery();
+                        }
+                        else
+                        {
+                            SqlCommand cartInsert = new SqlCommand("insert into CartData Values (@UserID, @artIDlbl, @quanSelected)", connect2);
+                            cartInsert.Parameters.AddWithValue("@UserID", Session["UserID"]);
+                            cartInsert.Parameters.AddWithValue("@artIDlbl", artId);
+                            cartInsert.Parameters.AddWithValue("@quanSelected", quanSelected);
+
+                            cartInsert.ExecuteNonQuery();
+                        }
+                        connect.Close();
+                        connect2.Close();
+                        validateQuanLbl.Style.Add("color", "green");
+                        validateQuanLbl.Text = "Successfully added to your cart!";
+                        Response.Write("<script language=javascript>alert('Successfully added to your cart!')</script>");
+                    }
+                    else if (quanSelected > checkStock)
+                    {
+                        validateQuanLbl.Text = "Please enter a quantity not more than available stock. Try again.";
+                        Response.Write("<script language=javascript>alert('Please enter a quantity not more than available stock. Try again.')</script>");
+                    }
+                    else
+                    {
+                        validateQuanLbl.Text = "Please enter a valid value (More than 0). Try again.";
+                        Response.Write("<script language=javascript>alert('Please enter a valid value (More than 0). Try again.')</script>");
+                    }
+                }
+
+            }
+
+        }
+
+        protected void btnAddWish_Click(object sender, EventArgs e)
+        {
+
+            int userid = Convert.ToInt32(Session["UserID"]);
+            foreach (DataListItem Item in artDetailsList.Items)
+            {
+                ImageButton btnAddWish = (ImageButton)Item.FindControl("btnAddWish");
+                Label artIDlbl = (Label)Item.FindControl("artIDlbl");
+
+                int artID = int.Parse(artIDlbl.Text);
+
+                string strCon = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+                SqlConnection connect;
+                connect = new SqlConnection(strCon);
+                connect.Open();
+                SqlDataAdapter da = new SqlDataAdapter("Select artID From WishlistData where artID=" + artID, connect);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                if (dt.Rows.Count.ToString() == "0")
+                {
+                    SqlCommand addWishlist = new SqlCommand("Insert into WishlistData Values (@UserID, @artIDlbl)", connect);
+                    addWishlist.Parameters.AddWithValue("@artIDlbl", artID);
+                    addWishlist.Parameters.AddWithValue("@UserID", Session["UserID"]);
+                    addWishlist.ExecuteNonQuery();
+                    Response.Write("<script>alert('The item is added in the wishlist')</script>");
+                    connect.Close();
+                }
+                else
+                {
+                    btnAddWish.ImageUrl = "media files/wishlistAdded.png";
+                    btnAddWish.Enabled = false;
+                    Response.Write("<script>alert('The item already exist in the wishlist')</script>");
+
+                }
+
+            }
+        }
+
+       
+
+
+    }
+}
