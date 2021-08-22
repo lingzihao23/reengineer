0ca5d9081cd444e5b9638daab891016ae1c06497
lingzihao23 <68987717+lingzihao23@users.noreply.github.com>
2021-08-23 00:42:23 +0800

    Add files via upload

---
+++ Menu.aspx
@@ -0,0 +1,96 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true" CodeBehind="Menu.aspx.cs" Inherits="Web_Asgm.CustomerGallery" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" Runat="server">
+    <link href="Menu.css" rel="stylesheet" type="text/css">
+
+
+
+
+
+    <br /><br /> <br /><br />
+
+    <div class="custGalleryFilter">
+        <hFilter>
+            Filter Juice By Category &nbsp;&nbsp; 
+        </hFilter>
+        <asp:DropDownList ID="custGalleryFilter" CssClass="filterDropdown" runat="server" OnSelectedIndexChanged="custGalleryFilter_SelectedIndexChanged">
+            <asp:ListItem>All</asp:ListItem>
+            <asp:ListItem>Natural</asp:ListItem>
+            <asp:ListItem>With Flavoring</asp:ListItem>
+        </asp:DropDownList>
+         
+        <hSort>
+            Sort Juice By &nbsp;&nbsp; 
+        </hSort>
+        <asp:DropDownList ID="custGallerySort" CssClass="sortDropdown" runat="server">
+            <asp:ListItem>None</asp:ListItem>
+            <asp:ListItem>Price Ascending</asp:ListItem>
+            <asp:ListItem>Price Descending</asp:ListItem>
+            <asp:ListItem>Stock Ascending</asp:ListItem>
+            <asp:ListItem>Stock Descending</asp:ListItem>
+        </asp:DropDownList>
+
+        <hConfirm>
+            <asp:Button ID="filterSortConfirm" CssClass="filterSortConfirm" runat="server" Text="Confirm" OnClick="filterSortConfirm_Click" />
+        </hConfirm>        
+
+    </div>
+                              
+    <br /><br /><br />
+
+    <div align="center">
+        <hNowViewing>
+            Viewing <hNowViewingLbl><asp:Label ID="custGalleryViewing" CssClass="custGalleryViewing" runat="server" Text=""></asp:Label></hNowViewingLbl> Menu
+        </hNowViewing>
+    </div>
+
+    <br />
+
+    <div class="galleryDisplay" align="center">
+        <asp:DataList ID="custGalleryList" runat="server" RepeatColumns="3" CellPadding="25"  OnItemCommand="custGalleryList_ItemCommand" RepeatDirection="Horizontal">
+            <ItemTemplate>
+                <table>
+                    <tr>
+                        <td colspan="2"><asp:Image Height="300px" Width="300px" runat="server" ImageUrl='<%#Eval("ArtPic") %>'></asp:Image></td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td  class="artInfo1" style="width:140px">Product Name: </td>
+                        <td  class="artInfo2" style="width:140px"><%#Eval("ArtName") %></td>
+                        <caption>
+                            <br />
+                        </caption>
+                    </tr>
+                    <tr>
+                        <td class="artInfo1">Price: </td>
+                        <td class="artInfo2">RM <%#Eval("Price") %></td>
+                        <caption>
+                            <br />
+                        </caption>
+                    </tr>
+                    <tr>
+                        <td class="artInfo1">Stock Available: </td>
+                        <td class="artInfo2"><%#Eval("Stock") %></td>
+                        <caption>
+                            <br />
+                        </caption>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" style="text-align:center" class="galleryBtns" >
+                            <asp:linkButton ID="btnViewDetails" runat="server" Text="View More Info" CommandName="viewDetails" CommandArgument='<% #Eval("ArtID") %>' />
+                        </td>
+                    </tr>
+                </table>
+            </ItemTemplate>
+        </asp:DataList>
+    </div>
+    <br /><br />
+
+</asp:Content>
+
+
---
+++ Menu.aspx.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Web_Asgm
+{
+    public partial class CustomerGallery : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                custGalleryViewing.Text = "All";
+                string strCon = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+                SqlConnection connect;
+                connect = new SqlConnection(strCon);
+
+                string selectQuery = "SELECT * FROM ArtGalleryData INNER JOIN UserDetail ON ArtGalleryData.UserID = UserDetail.UserID";
+
+                SqlCommand artSelect = new SqlCommand(selectQuery, connect);
+                connect.Open();
+
+                SqlDataAdapter artAdapt = new SqlDataAdapter();
+                artAdapt.SelectCommand = artSelect;
+                DataSet artSet = new DataSet();
+                artAdapt.Fill(artSet);
+                custGalleryList.DataSource = artSet;
+                custGalleryList.DataBind();
+
+                connect.Close();
+            }
+            else
+            {
+                string strCon = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+                SqlConnection connect;
+                connect = new SqlConnection(strCon);
+                string selectedCategory = custGalleryFilter.SelectedItem.Value;
+                string selectedSort = custGallerySort.SelectedItem.Value;
+                string combinedQuery = "";
+                custGalleryViewing.Text = selectedCategory; 
+
+                if (selectedCategory == "All")
+                {
+                    if (selectedSort == "None")
+                    {
+                        combinedQuery = "SELECT * FROM ArtGalleryData INNER JOIN UserDetail ON ArtGalleryData.UserID = UserDetail.UserID";
+                    }
+                    else if (selectedSort == "Price Ascending")
+                    {
+                        combinedQuery = "SELECT * FROM ArtGalleryData INNER JOIN UserDetail ON ArtGalleryData.UserID = UserDetail.UserID ORDER BY Price";
+                    }
+                    else if (selectedSort == "Price Descending")
+                    {
+                        combinedQuery = "SELECT * FROM ArtGalleryData INNER JOIN UserDetail ON ArtGalleryData.UserID = UserDetail.UserID ORDER BY Price DESC";
+                    }
+                    else if (selectedSort == "Stock Ascending")
+                    {
+                        combinedQuery = "SELECT * FROM ArtGalleryData INNER JOIN UserDetail ON ArtGalleryData.UserID = UserDetail.UserID ORDER BY Stock";
+                    }
+                    else if (selectedSort == "Stock Descending")
+                    {
+                        combinedQuery = "SELECT * FROM ArtGalleryData INNER JOIN UserDetail ON ArtGalleryData.UserID = UserDetail.UserID ORDER BY Stock DESC";
+                    }
+
+                    connect.Open();
+                    string selectQuery = combinedQuery;
+                    SqlCommand artSelect = new SqlCommand(selectQuery, connect);
+
+                    SqlDataAdapter artAdapt = new SqlDataAdapter();
+                    artAdapt.SelectCommand = artSelect;
+                    DataSet artSet = new DataSet();
+                    artAdapt.Fill(artSet);
+                    custGalleryList.DataSource = artSet;
+                    custGalleryList.DataBind();
+
+                    connect.Close();
+                }
+                else
+                {
+                    if (selectedSort == "None")
+                    {
+                        combinedQuery = "SELECT * FROM ArtGalleryData INNER JOIN UserDetail ON ArtGalleryData.UserID = UserDetail.UserID WHERE ArtGalleryData.Category = '" + selectedCategory + "'";
+                    }
+                    else if (selectedSort == "Price Ascending")
+                    {
+                        combinedQuery = "SELECT * FROM ArtGalleryData INNER JOIN UserDetail ON ArtGalleryData.UserID = UserDetail.UserID WHERE ArtGalleryData.Category = '" + selectedCategory + "'" + " ORDER BY Price";
+                    }
+                    else if (selectedSort == "Price Descending")
+                    {
+                        combinedQuery = "SELECT * FROM ArtGalleryData INNER JOIN UserDetail ON ArtGalleryData.UserID = UserDetail.UserID WHERE ArtGalleryData.Category = '" + selectedCategory + "'" + " ORDER BY Price DESC";
+                    }
+                    else if (selectedSort == "Stock Ascending")
+                    {
+                        combinedQuery = "SELECT * FROM ArtGalleryData INNER JOIN UserDetail ON ArtGalleryData.UserID = UserDetail.UserID WHERE ArtGalleryData.Category = '" + selectedCategory + "'" + " ORDER BY Stock";
+                    }
+                    else if (selectedSort == "Stock Descending")
+                    {
+                        combinedQuery = "SELECT * FROM ArtGalleryData INNER JOIN UserDetail ON ArtGalleryData.UserID = UserDetail.UserID WHERE ArtGalleryData.Category = '" + selectedCategory + "'" + " ORDER BY Stock DESC";
+                    }
+
+                    connect.Open();
+                    string selectQuery = combinedQuery;
+                    SqlCommand artSelect = new SqlCommand(selectQuery, connect);
+
+                    SqlDataAdapter artAdapt = new SqlDataAdapter();
+                    artAdapt.SelectCommand = artSelect;
+                    DataSet artSet = new DataSet();
+                    artAdapt.Fill(artSet);
+                    custGalleryList.DataSource = artSet;
+                    custGalleryList.DataBind();
+
+                    connect.Close();
+                }
+            }
+        }
+
+        protected void custGalleryList_ItemCommand(object source, DataListCommandEventArgs e)
+        {
+            if(e.CommandName== "viewDetails")
+            {
+                //Response.Write("<script language=javascript>alert('"+ e.CommandArgument.ToString() +"') </script>");
+                Response.Redirect("MenuDetails.aspx?id=" + e.CommandArgument.ToString());
+            }
+        }
+
+        protected void custGalleryFilter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        protected void filterSortConfirm_Click(object sender, EventArgs e)
+        {
+            string strCon = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            SqlConnection connect;
+            connect = new SqlConnection(strCon);
+            string selectedCategory = custGalleryFilter.SelectedItem.Value;
+            string selectedSort = custGallerySort.SelectedItem.Value;
+            string combinedQuery = "";
+            custGalleryViewing.Text = selectedCategory;
+
+            if (selectedCategory == "All")
+            {
+                if (selectedSort == "None")
+                {
+                    combinedQuery = "SELECT * FROM ArtGalleryData INNER JOIN UserDetail ON ArtGalleryData.UserID = UserDetail.UserID";
+                }
+                else if (selectedSort == "Price Ascending")
+                {
+                    combinedQuery = "SELECT * FROM ArtGalleryData INNER JOIN UserDetail ON ArtGalleryData.UserID = UserDetail.UserID ORDER BY Price";
+                }
+                else if (selectedSort == "Price Descending")
+                {
+                    combinedQuery = "SELECT * FROM ArtGalleryData INNER JOIN UserDetail ON ArtGalleryData.UserID = UserDetail.UserID ORDER BY Price DESC";
+                }
+                else if (selectedSort == "Stock Ascending")
+                {
+                    combinedQuery = "SELECT * FROM ArtGalleryData INNER JOIN UserDetail ON ArtGalleryData.UserID = UserDetail.UserID ORDER BY Stock";
+                }
+                else if (selectedSort == "Stock Descending")
+                {
+                    combinedQuery = "SELECT * FROM ArtGalleryData INNER JOIN UserDetail ON ArtGalleryData.UserID = UserDetail.UserID ORDER BY Stock DESC";
+                }
+
+                connect.Open();
+                string selectQuery = combinedQuery;
+                SqlCommand artSelect = new SqlCommand(selectQuery, connect);
+                
+                SqlDataAdapter artAdapt = new SqlDataAdapter();
+                artAdapt.SelectCommand = artSelect;
+                DataSet artSet = new DataSet();
+                artAdapt.Fill(artSet);
+                custGalleryList.DataSource = artSet;
+                custGalleryList.DataBind();
+
+                connect.Close();
+            }
+            else
+            {
+                if (selectedSort == "None")
+                {
+                    combinedQuery = "SELECT * FROM ArtGalleryData INNER JOIN UserDetail ON ArtGalleryData.UserID = UserDetail.UserID WHERE ArtGalleryData.Category = '" + selectedCategory + "'";
+                }
+                else if (selectedSort == "Price Ascending")
+                {
+                    combinedQuery = "SELECT * FROM ArtGalleryData INNER JOIN UserDetail ON ArtGalleryData.UserID = UserDetail.UserID WHERE ArtGalleryData.Category = '" + selectedCategory + "'" + " ORDER BY Price";
+                }
+                else if (selectedSort == "Price Descending")
+                {
+                    combinedQuery = "SELECT * FROM ArtGalleryData INNER JOIN UserDetail ON ArtGalleryData.UserID = UserDetail.UserID WHERE ArtGalleryData.Category = '" + selectedCategory + "'" + " ORDER BY Price DESC";
+                }
+                else if (selectedSort == "Stock Ascending")
+                {
+                    combinedQuery = "SELECT * FROM ArtGalleryData INNER JOIN UserDetail ON ArtGalleryData.UserID = UserDetail.UserID WHERE ArtGalleryData.Category = '" + selectedCategory + "'" + " ORDER BY Stock";
+                }
+                else if (selectedSort == "Stock Descending")
+                {
+                    combinedQuery = "SELECT * FROM ArtGalleryData INNER JOIN UserDetail ON ArtGalleryData.UserID = UserDetail.UserID WHERE ArtGalleryData.Category = '" + selectedCategory + "'" + " ORDER BY Stock DESC";
+                }
+
+                connect.Open();
+                string selectQuery = combinedQuery;
+                SqlCommand artSelect = new SqlCommand(selectQuery, connect);
+                
+                SqlDataAdapter artAdapt = new SqlDataAdapter();
+                artAdapt.SelectCommand = artSelect;
+                DataSet artSet = new DataSet();
+                artAdapt.Fill(artSet);
+                custGalleryList.DataSource = artSet;
+                custGalleryList.DataBind();
+
+                connect.Close();
+            }
+        }
+    }
+}
