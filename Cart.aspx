66470bb05200d3a61b5806c2f05341e7ad90e37c
seanlau2415 <lauja-wm18@student.tarc.edu.my>
2021-08-23 00:59:51 +0800

    Add files via upload

---
+++ Cart.aspx
@@ -0,0 +1,82 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/Customer.master" CodeBehind="Cart.aspx.cs" Inherits="Web_Asgm.Cart" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" Runat="server">
+
+    <link href="Cart.css" rel="stylesheet" type="text/css">
+
+    <br /><br /><br /><br /><br />
+
+    <div align="center">
+          <hCartlbl>
+              <asp:Label ID="viewCartlbl" runat="server" Text="Your Cart"></asp:Label>
+          </hCartlbl>
+    </div>
+    <br /><br />
+
+    <div class="cartDisplay" align="center">     
+               
+        <asp:DataList ID="cartDisplayList" runat="server" DataSourceID="SqlDataSource1" CellSpacing="70">
+            <ItemTemplate>
+                <table class="cartTable">
+                    <tr>
+                        <td rowspan="7" style="width:40%"><asp:Image Height="280px" Width="280px" runat="server" ImageUrl='<%#Eval("ArtPic") %>' BorderStyle="Ridge" BorderColor="Gray" BorderWidth="5px"></asp:Image></td>
+                    </tr>
+                    <tr>
+                        <td><br /></td>
+                    </tr>
+                    <tr>
+                        <td class="cartInfo" style="width:40%">Juice Name:</td>
+                        <td class="cartInfo"><asp:Label ID="JuiceNameLabel" runat="server" Text='<%# Eval("ArtName") %>' /></td>
+                        <td class="cartInfo" style="visibility:hidden"><asp:Label ID="JuiceIDlbl" runat="server" Text='<%# Eval("ArtID") %>' /></td>
+                        
+                    </tr>
+                    <tr>
+                        <td class="cartInfo">Stock Available: </td>
+                        <td class="cartInfo"><asp:Label ID="stockLbl" runat="server" Text='<%# Eval("Stock") %>' /></td>
+                        <td class ="removeBtnCell">
+                            <asp:ImageButton ID="removeCartbtn" CssClass="removeBtn" runat="server" OnClientClick="return confirm('Remove this item from your cart?')" CommandArgument='<%#Eval("ArtID") + ";" +Eval("UserID")%>' OnCommand="removeCartbtn_Command"  ImageUrl="~/media files/removeBtn1.png" onmouseover="this.src='/media files/removeBtn2.png'" onmouseout="this.src='/media files/removeBtn1.png'"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="cartInfo">Quantity Selected:</td>
+                        <td><asp:TextBox ID="cartQuantity" runat="server" TextMode="Number" Value='<%# Eval("quantity") %>' AutoPostBack="True" Width="40%"></asp:TextBox></td>
+                    </tr>
+                    <tr>
+                        <td class="cartInfo">Unit Price:</td>
+                        <td class="cartInfo">$<asp:Label ID="PriceLabel" runat="server" Text='<%#Eval("Price") %>' /></td>
+                    </tr>
+                    <tr>
+                        <td class="cartInfo" style="visibility:hidden"><asp:Label ID="empty" runat="server" Text='Nothing' /></td>
+                        <td class="cartInfo"><asp:Label ID="quantityLabel" runat="server" Text='<%# Eval("quantity") %>' Visible="False" /></td>
+                    </tr>
+                </table>
+            </ItemTemplate>
+            <FooterTemplate>
+                <asp:Label Visible='<%#bool.Parse((cartDisplayList.Items.Count==0).ToString())%>' 
+                           runat="server" ID="lblCartEmpty" class="cartEmpty" Text="<br/><br/><br/><br/><br/><br/>Your Cart is Empty!<br/><br/><br/><br/><br/><br/>"></asp:Label>
+            </FooterTemplate>
+        </asp:DataList>
+
+        <div align="right">
+            <asp:Label ID="grandTotal" class="grandtotallbl" runat="server" Text=""></asp:Label>
+        </div>    
+        
+        <br /><br />
+
+        <div align="right">
+            <asp:Button ID="checkoutBtn" class="checkoutBtn" runat="server" Text="Confirm and Checkout" OnClientClick="return confirm('Confirm and Proceed to Checkout?')" OnCommand="checkOutbtn_Command"/>
+        </div>
+        
+        <br />
+    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" SelectCommand="SELECT CartData.quantity, CartData.UserID, ArtGalleryData.ArtID, ArtGalleryData.ArtPic, ArtGalleryData.ArtName, ArtGalleryData.Stock, ArtGalleryData.Price, UserDetail.FirstName FROM ((CartData INNER JOIN ArtGalleryData ON CartData.ArtID = ArtGalleryData.ArtID) INNER JOIN UserDetail ON CartData.UserID = UserDetail.UserID) WHERE UserDetail.UserID = @UserID">
+        <SelectParameters>                                                                                                            
+            <asp:SessionParameter Name="UserID" SessionField="UserID" />
+        </SelectParameters>
+        </asp:SqlDataSource>
+        <br /><br />
+    </div>
+    </asp:Content>
+
+
+
+
---
+++ Cart.aspx.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Web_Asgm
+{
+    public partial class Cart : System.Web.UI.Page
+    {
+        bool cartError = false;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["UserID"] == null)
+            {
+                Response.Redirect("Login.aspx");
+            }
+
+            int quantityPriceCart;
+            int temp = 0;
+            int temp2 = 0;
+            
+            foreach (DataListItem Item in cartDisplayList.Items)
+            {
+                if (!IsPostBack)
+                {
+                    Label quantityLabel = (Label)Item.FindControl("quantityLabel");
+                    Label PriceLabel = (Label)Item.FindControl("PriceLabel");
+
+                    int quantityCart = int.Parse(quantityLabel.Text);
+                    int priceCart;
+
+                    priceCart = int.Parse(PriceLabel.Text.Substring(0, PriceLabel.Text.IndexOf('.')));
+
+                    quantityPriceCart = quantityCart * priceCart;
+                    temp2 += quantityCart;
+                    temp += quantityPriceCart;
+
+                    int grandTotalAmt;
+                    grandTotalAmt = temp;
+                    grandTotal.Text = "Grand Total: RM " + grandTotalAmt.ToString() + ".00";
+                }
+                else
+                {
+                    TextBox cartQuantityText = (TextBox)Item.FindControl("cartQuantity");
+                    Label PriceLabel = (Label)Item.FindControl("PriceLabel");
+                    Label JuiceIDlbl = (Label)Item.FindControl("JuiceIDlbl");
+                    Label viewStocklbl = (Label)Item.FindControl("stockLbl");
+                    int juiceId = int.Parse(JuiceIDlbl.Text);
+                    int quantityCart;
+                    
+                    int checkStock = int.Parse(viewStocklbl.Text);
+
+                    if (cartQuantityText.Text == string.Empty)
+                    {
+                        Response.Write("<script language=javascript>alert('No value entered. Try again.')</script>");
+                        cartError = true;
+                        if (int.TryParse(cartQuantityText.Text.Trim(), out quantityCart))
+                        {
+
+                        }
+
+                        int priceCart;
+
+                        priceCart = int.Parse(PriceLabel.Text.Substring(0, PriceLabel.Text.IndexOf('.')));
+
+                        quantityPriceCart = quantityCart * priceCart;
+                        temp2 += quantityCart;
+                        temp += quantityPriceCart;
+
+                        string strCon = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+                        SqlConnection connect;
+                        connect = new SqlConnection(strCon);
+                        connect.Open();
+                        SqlCommand cartUpdate = new SqlCommand("Update CartData Set quantity =" + 0 + "where UserID = " + Session["UserID"] + "and ArtID = " + juiceId, connect);
+                        cartUpdate.ExecuteNonQuery();
+                        connect.Close();
+                        cartQuantityText.Text = "0";
+
+                        int grandTotalAmt;
+                        grandTotalAmt = temp;
+                        grandTotal.Text = "Grand Total: RM " + grandTotalAmt.ToString() + ".00";
+                    }
+                    else
+                    {
+                        int cartQuanSelected = int.Parse(cartQuantityText.Text);
+                        if (cartQuanSelected <= checkStock && cartQuanSelected > 0 && cartQuanSelected != 0)
+                        {
+                            if (int.TryParse(cartQuantityText.Text.Trim(), out quantityCart))
+                            {
+
+                            }
+
+                            int priceCart;
+
+                            priceCart = int.Parse(PriceLabel.Text.Substring(0, PriceLabel.Text.IndexOf('.')));
+
+                            quantityPriceCart = quantityCart * priceCart;
+                            temp2 += quantityCart;
+                            temp += quantityPriceCart;
+                            string strCon = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+                            SqlConnection connect;
+                            connect = new SqlConnection(strCon);
+                            connect.Open();
+                            SqlCommand cartUpdate = new SqlCommand("Update CartData Set quantity =" + quantityCart + "where UserID = " + Session["UserID"] + "and ArtID = " + juiceId, connect);
+                            cartUpdate.ExecuteNonQuery();
+                            connect.Close();
+                            int grandTotalAmt;
+                            int grandQuanAmt;
+                            grandQuanAmt = temp2;
+                            grandTotalAmt = temp;
+                            Session["grandTotalVal"] = grandTotalAmt;
+                            Session["grandQuantity"] = grandQuanAmt;
+                            grandTotal.Text = "Grand Total: RM " + grandTotalAmt.ToString() + ".00";
+                        }
+                        else if (cartQuanSelected > checkStock)
+                        {
+                            Response.Write("<script language=javascript>alert('Please enter a quantity not more than available stock. Try again.')</script>");
+                            cartError = true;
+                        }
+                        else
+                        {
+                            Response.Write("<script language=javascript>alert('Please enter a valid value (More than 0). Try again.')</script>");
+                            cartError = true;
+                        }
+                    }                 
+                }               
+            }
+        }
+
+        protected void removeCartbtn_Command(object sender, CommandEventArgs e)
+        {
+            foreach (DataListItem Item in cartDisplayList.Items)
+            {
+                try
+                {
+                    string strCon = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+                    SqlConnection connect;
+                    connect = new SqlConnection(strCon);
+
+                    connect.Open();
+
+                    string[] commandArgs = e.CommandArgument.ToString().Split(new char[] { ';' });
+                    string juiceId = commandArgs[0];
+                    string userId = commandArgs[1];
+
+                    SqlCommand cartDelete = new SqlCommand("DELETE FROM CartData WHERE UserID =" + userId + "AND ArtID =" + juiceId, connect);
+
+                    cartDelete.ExecuteNonQuery();
+                    connect.Close();
+
+                    Response.Write("<script>alert('Item has been removed from your cart')</script>");
+                    Response.Write("<script>window.location.href='Cart.aspx';</script>");
+                }
+                catch (Exception)
+                {
+                    Response.Write("<script>alert('An error occured while trying to remove this item from your cart!')</script>");
+                }
+                
+            }
+        }
+
+        protected void checkOutbtn_Command(object sender, CommandEventArgs e)
+        {
+
+            foreach (DataListItem Item in cartDisplayList.Items)
+            {
+                    if (cartError == true)
+                    {
+                        
+                    }
+                    else
+                    {
+                        Response.Redirect("PaymentGateway.aspx?grandTotal=" + Session["grandTotalVal"] + "&grandQuan=" + Session["grandQuantity"]);
+                    }                
+            }           
+        }
+    }
+}
